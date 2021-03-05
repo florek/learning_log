fc9884c1cb28d8f0dfb2402cd876bae871aa8f3f
Paweł Florczak <pawel.florczak87@gmail.com>
2021-03-05 12:25:28 +0100

    2021.03.05

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Entry, Topic
 from .forms import TopicForm, EntryForm
 
 
+@login_required
 def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
@@ -25,6 +27,7 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
+@login_required
 def new_entry(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     if request.method != 'POST':
@@ -40,6 +43,7 @@ def new_entry(request, topic_id):
     return render(request, 'learning_logs/new_entry.html', context)
 
 
+@login_required
 def new_topic(request):
     if request.method != 'POST':
         form = TopicForm()
@@ -52,6 +56,7 @@ def new_topic(request):
     return render(request, 'learning_logs/new_topic.html', context)
 
 
+@login_required
 def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {
@@ -60,6 +65,7 @@ def topics(request):
     return render(request, 'learning_logs/topics.html', context)
 
 
+@login_required
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
